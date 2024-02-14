a62dfd2dcf6405e23826c8d2774fd235bf909d97
leefarhadaman <leefarhadaman786@gmail.com>
2024-02-14 22:26:09 +0530

    first commit

---
+++ lib/View/io.dart
@@ -0,0 +1,88 @@
+import 'dart:async';
+import 'package:crypto/View/splash.dart';
+import 'package:flutter/material.dart';
+
+class IO extends StatefulWidget {
+  const IO({Key? key}) : super(key: key);
+
+  @override
+  State<IO> createState() => _IOState();
+}
+
+class _IOState extends State<IO> {
+  @override
+  void initState() {
+    Timer(
+      Duration(seconds: 4),
+          () {
+        Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => Splash()));
+      },
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double myHeight = MediaQuery.of(context).size.height;
+    double myWidth = MediaQuery.of(context).size.width;
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color(0xffFBC700),
+        body: Container(
+          height: myHeight,
+          width: myWidth,
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: myHeight * 0.05),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(''),
+                Text(
+                  'Faru Crypto',
+                  style: TextStyle(
+                      fontSize: 60,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold),
+                ),
+                Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          'Created by',
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.black,
+                              fontWeight: FontWeight.normal),
+                        ),
+                        SizedBox(
+                          width: myWidth * 0.02,
+                        ),
+                        Text(
+                          "Farhad",
+                            style: TextStyle( fontSize: myHeight * 0.02,fontWeight: FontWeight.bold)
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: myHeight * 0.005,
+                    ),
+                    Image.asset(
+                      'assets/image/loading1.gif',
+                      height: myHeight * 0.015,
+                      color: Colors.black,
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
